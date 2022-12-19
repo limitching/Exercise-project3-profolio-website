5d77807db721532579316dd5d17d845153913cbb
HUNG WEI-CHING <hwc0320@gmail.com>
2022-12-19 17:47:15 +0800

    Add pure bubble effect in CSS

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+let nav = document.querySelector("nav");
+
 window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
+  if (window.scrollY == 0) {
+    nav.style.boxShadow = "";
+  } else {
+    nav.style.boxShadow = "0 10px 6px -6px #777";
+  }
 });
